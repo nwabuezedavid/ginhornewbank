819b6adaf5a29fb817d6f7d98fa4027c72d7edf4
NwabuezeDavid <131411341+nwabuezedavid@users.noreply.github.com>
2025-10-23 14:25:05 +0100

    Update settings.py

--- banking/settings.py
+++ banking/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 # settings.py
-CSRF_COOKIE_AGE = 60 * 60 * 12  # 12 hours
+CSRF_COOKIE_AGE = 60 * 60 * 12
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...) 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
